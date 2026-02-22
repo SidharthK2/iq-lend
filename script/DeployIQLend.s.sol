b277e9350a2ca07133ae7be5ae4f33d1d15e8c49
SidharthK2 <sidharthkapoor.100@gmail.com>
2026-02-22 17:55:46 +0530

    update deps

--- script/DeployIQLend.s.sol
+++ script/DeployIQLend.s.sol
@@ -2,30 +2,26 @@
 pragma solidity >=0.8.19;
 
 import { IQLend } from "../src/IQLend.sol";
-import { AdaptiveCurveIrm } from "@morpho-blue-irm/adaptive-curve-irm/AdaptiveCurveIrm.sol";
 import { Constants } from "../src/Constants.sol";
 import { Script, console } from "forge-std/Script.sol";
 
+/// @notice Deploys IQLend and enables the already-deployed IRM and LLTV.
+/// @dev The AdaptiveCurveIrm is already deployed at Constants.IRM.
 contract DeployIQLend is Script {
-    function run() external returns (address, address) {
+    function run() external returns (address) {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address deployer = vm.addr(deployerPrivateKey);
 
         vm.startBroadcast(deployerPrivateKey);
 
-        IQLend iqLend = new IQLend(deployer);
+        IQLend iqLend = new IQLend(vm.addr(deployerPrivateKey));
 
-        // Deploy IRM
-        AdaptiveCurveIrm irm = new AdaptiveCurveIrm(address(iqLend));
-
-        // Enable IRM and LLTV
-        iqLend.enableIrm(address(irm));
+        // Enable already-deployed IRM and LLTV
+        iqLend.enableIrm(Constants.IRM);
         iqLend.enableLltv(Constants.LLTV);
 
         console.log("IQLend", address(iqLend));
-        console.log("IRM", address(irm));
         vm.stopBroadcast();
 
-        return (address(iqLend), address(irm));
+        return address(iqLend);
     }
 }
